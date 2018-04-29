6717572c245e5cf9c5caf9e7db18d27e82e663a8
ephemeral9794 <kthm9794.hiro@gmail.com>
2018-04-29 18:47:52 +0900

    Load 'meshes' and Update README.md
    Load 'meshes' in JSON
    Update README.md

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # VRMLoader
 
 ## VRMとは
---[dwango on GitHub/VRM](https://dwango.github.io/vrm/)より引用--
+-- [dwango on GitHub/VRM](https://dwango.github.io/vrm/)より引用 --
 
 「VRM」はVRアプリケーション向けの人型3Dアバター（3Dモデル）データを扱うためのファイルフォーマットです。
 [glTF2.0](https://www.khronos.org/gltf/)をベースとしており、誰でも自由に利用することができます。
 
---引用終わり--
+-- 引用終わり --
 
 ## VRMLoaderについて
 VRMLoaderは自作VRM Viewerのための自作ライブラリです。
@@ -15,8 +15,10 @@ VRMLoaderは自作VRM Viewerのための自作ライブラリです。
 このライブラリは[MIT License](./LICENSE)を適用しています。
 
 以下のサードパーティ製ライブラリを使用しています。
-* [JSON.NET](https://www.newtonsoft.com/json):Copyright (c) 2007 James Newton-King
+
+* [JSON.NET](https://www.newtonsoft.com/json) : Copyright (c) 2007 James Newton-King
 
 ## Special Thanks
 * [glTF Homepage](https://www.khronos.org/gltf/)
 * [glTF 2.0 Specification](https://github.com/KhronosGroup/glTF/)
+* [glTF-loader](https://github.com/Lugdunum3D/glTF2-loader)
--- VRMLoader/VRM.cs
+++ VRMLoader/VRM.cs
@@ -2,16 +2,59 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Collections;
 using Newtonsoft.Json.Linq;
 using VRMLoader.Utility;
+using static VRMLoader.GLenum;
+using System.Collections.Generic;
 
 namespace VRMLoader
 {
+	public enum GLenum {
+		GL_BYTE = 0x1400,
+		GL_UNSIGNED_BYTE = 0x1401,
+		GL_SHORT = 0x1402,
+		GL_UNSIGNED_SHORT = 0x1403,
+		GL_INT = 0x1404,
+		GL_UNSIGNED_INT = 0x1405,
+		GL_FLOAT = 0x1406,
+		GL_2_BYTES = 0x1407,
+		GL_3_BYTES = 0x1408,
+		GL_4_BYTES = 0x1409,
+		GL_DOUBLE = 0x140A,
+
+		/* TextureMagFilter */
+		GL_NEAREST = 0x2600,
+		GL_LINEAR = 0x2601,
+
+		/* TextureMinFilter */
+		GL_NEAREST_MIPMAP_NEAREST = 0x2700,
+		GL_LINEAR_MIPMAP_NEAREST = 0x2701,
+		GL_NEAREST_MIPMAP_LINEAR = 0x2702,
+		GL_LINEAR_MIPMAP_LINEAR = 0x2703,
+
+		/* TextureWrapMode */
+		GL_CLAMP = 0x2900,
+		GL_REPEAT = 0x2901,
+
+		/* BeginMode */
+		GL_POINTS = 0x0000,
+		GL_LINES = 0x0001,
+		GL_LINE_LOOP = 0x0002,
+		GL_LINE_STRIP = 0x0003,
+		GL_TRIANGLES = 0x0004,
+		GL_TRIANGLE_STRIP = 0x0005,
+		GL_TRIANGLE_FAN = 0x0006,
+		GL_QUADS = 0x0007,
+		GL_QUAD_STRIP = 0x0008,
+		GL_POLYGON = 0x0009,
+	};
+
 	struct VRMAsset {
-        public string copyright;
-        public string generator;
-        public string version;
-        public string minVersion;
+		public string copyright;
+		public string generator;
+		public string version;
+		public string minVersion;
 
 		public override string ToString()
 		{
@@ -36,6 +79,55 @@ namespace VRMLoader
 				builder.AppendLine();
 				builder.AppendFormat("  node #{0:000} : {1}", i, n);
 				i++;
+			}
+			return builder.ToString();
+		}
+	}
+	struct VRMPrimitive {
+		public int indices;
+		public int material;
+		public int mode;
+		public Dictionary<string, int> attributes;
+		public Dictionary<string, int>[] targets;
+
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder();
+			builder.AppendFormat("    indices  : {0}", indices).AppendLine();
+			builder.AppendFormat("    material : {0}", material).AppendLine();
+			builder.AppendFormat("    mode     : {0}", mode);
+			foreach (var at in attributes) {
+				builder.AppendLine();
+				builder.AppendFormat("    attributes {0} : {1}", at.Key, at.Value);
+			}
+			for (int i = 0; i < targets.Length; i++) {
+				builder.AppendLine();
+				builder.AppendFormat("    targets #{0:000}", i);
+				foreach (var at in targets[i]) {
+					builder.AppendLine();
+					builder.AppendFormat("      {0} : {1}", at.Key, at.Value);
+				}
+			}
+			return builder.ToString();
+		}
+	}
+	struct VRMMesh {
+		public string name;
+		public float[] weights;
+		public VRMPrimitive[] primitives;
+
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder();
+			builder.AppendFormat("  name      : {0}", name);
+			for (int i = 0; i < weights.Length; i++) {
+				builder.AppendLine();
+				builder.AppendFormat("  weight #{0:000} : {1}", i, weights[i]);
+			}
+			for (int i = 0; i < primitives.Length; i++) {
+				builder.AppendLine();
+				builder.AppendFormat("  primitive #{0:000}", i).AppendLine();
+				builder.AppendFormat("{0}", primitives[i].ToString());
 			}
 			return builder.ToString();
 		}
@@ -99,25 +191,25 @@ namespace VRMLoader
 					JObject asset = (JObject)root["asset"];
 					// version
 					if (asset.ContainsKey("version")) {
-						vrm.asset.version = asset.Value<String>("version");
+						vrm.asset.version = (string)asset["version"];
 					} else {
 						vrm.asset.version = "";
 					}
 					// copyright
 					if (asset.ContainsKey("copyright")) {
-						vrm.asset.copyright = asset.Value<String>("copyright");
+						vrm.asset.copyright = (string)asset["copyright"];
 					} else {
 						vrm.asset.copyright = "";
 					}
 					// generator
 					if (asset.ContainsKey("generator")) {
-						vrm.asset.generator = asset.Value<String>("generator");
+						vrm.asset.generator = (string)asset["generator"];
 					} else {
 						vrm.asset.generator = "";
 					}
 					// minVersion
 					if (asset.ContainsKey("minVersion")) {
-						vrm.asset.minVersion = asset.Value<String>("minVersion");
+						vrm.asset.minVersion = (string)asset["minVersion"];
 					} else {
 						vrm.asset.minVersion = "";
 					}
@@ -125,21 +217,26 @@ namespace VRMLoader
 
 				// scenes
 				if (root.ContainsKey("scenes")) {
-					JArray scenes = root.Value<JArray>("scenes");
+					JArray scenes = (JArray)root["scenes"];
 					vrm.scenes = new VRMScene[scenes.Count];
+
 					for(int i = 0; i < scenes.Count; i++) {
-						JObject scene = scenes.Value<JObject>(i);
+						JObject scene = (JObject)scenes[i];
 						vrm.scenes[i] = new VRMScene();
+
+						// name
 						if (scene.ContainsKey("name")) {
-							vrm.scenes[i].name = scene.Value<string>("name");
+							vrm.scenes[i].name = (string)scene["name"];
 						} else {
 							vrm.scenes[i].name = "";
 						}
+
+						// nodes
 						if (scene.ContainsKey("nodes")) {
-							JArray nodes = scene.Value<JArray>("nodes");
+							JArray nodes = (JArray)scene["nodes"];
 							vrm.scenes[i].nodes = new int[nodes.Count];
 							for (int j = 0; j < nodes.Count; j++) {
-								vrm.scenes[i].nodes[j] = nodes.Value<int>(j);
+								vrm.scenes[i].nodes[j] = (int)nodes[j];
 							}
 						} else {
 							vrm.scenes[i].nodes = (int[])Enumerable.Empty<int>();
@@ -147,6 +244,91 @@ namespace VRMLoader
 					}
 				} else {
 					vrm.scenes = (VRMScene[])Enumerable.Empty<VRMScene>();
+				}
+
+				// meshes
+				if (root.ContainsKey("meshes")) {
+					JArray meshes = (JArray)root["meshes"];
+					vrm.meshes = new VRMMesh[meshes.Count];
+
+					for (int i = 0; i < meshes.Count; i++) {
+						JObject mesh = (JObject)meshes[i];
+						vrm.meshes[i] = new VRMMesh();
+
+						// name
+						if (mesh.ContainsKey("name")) {
+							vrm.meshes[i].name = (string)mesh["name"];
+						} else {
+							vrm.meshes[i].name = "";
+						}
+
+						// weights
+						if (mesh.ContainsKey("weights")) {
+							JArray weights = (JArray)mesh["weights"];
+							vrm.meshes[i].weights = new float[weights.Count];
+							for (int j = 0; j < weights.Count; j++) {
+								vrm.meshes[i].weights[j] = (float)weights[j];
+							}
+						} else {
+							vrm.meshes[i].weights = (float[])Enumerable.Empty<float>();
+						}
+
+						// primitives
+						JArray primitives = (JArray)mesh["primitives"];
+						vrm.meshes[i].primitives = new VRMPrimitive[primitives.Count];
+						for (int j = 0; j < primitives.Count; j++) {
+							JObject primitive = (JObject)primitives[j];
+
+							// indices
+							if (primitive.ContainsKey("indices")) {
+								vrm.meshes[i].primitives[j].indices = (int)primitive["indices"];
+							} else {
+								vrm.meshes[i].primitives[j].indices = -1;
+							}
+
+							// material
+							if (primitive.ContainsKey("material")) {
+								vrm.meshes[i].primitives[j].material = (int)primitive["material"];
+							} else {
+								vrm.meshes[i].primitives[j].material = -1;
+							}
+
+							// mode
+							if (primitive.ContainsKey("mode")) {
+								vrm.meshes[i].primitives[j].mode = (int)primitive["mode"];
+							} else {
+								vrm.meshes[i].primitives[j].mode = -1;
+							}
+
+							// attributes
+							if (primitive.ContainsKey("attributes")) {
+								JObject attributes = (JObject)primitive["attributes"];
+								vrm.meshes[i].primitives[j].attributes = new Dictionary<string, int>();
+								foreach (var at in attributes) {
+									vrm.meshes[i].primitives[j].attributes.Add(at.Key, (int)at.Value);
+								}
+							} else {
+								vrm.meshes[i].primitives[j].attributes = new Dictionary<string, int>();
+							}
+
+							// targets
+							if (primitive.ContainsKey("targets")) {
+								JArray targets = (JArray)primitive["targets"];
+								vrm.meshes[i].primitives[j].targets = new Dictionary<string, int>[targets.Count];
+								for (int n = 0; n < targets.Count; n++) {
+									JObject target = (JObject)targets[n];
+									vrm.meshes[i].primitives[j].targets[n] = new Dictionary<string, int>();
+									foreach(var t in target) {
+										if (t.Value.Type == JTokenType.Integer) {
+											vrm.meshes[i].primitives[j].targets[n].Add(t.Key, (int)(t.Value));
+										}
+									}
+								}
+							}
+						}
+					}
+				} else {
+					vrm.meshes = (VRMMesh[])Enumerable.Empty<VRMMesh>();
 				}
 			} else {
 				throw new InvalidDataException("This JSON file does not contain a value.");
@@ -160,6 +342,7 @@ namespace VRMLoader
 
 		VRMAsset asset;
 		VRMScene[] scenes;
+		VRMMesh[] meshes;
 
 		#if DEBUG
 		public void Print() {
@@ -182,6 +365,12 @@ namespace VRMLoader
 				i++;
 			}
 			DebugLog.WriteLine();
+			i = 0;
+			foreach(var mesh in meshes) {
+				DebugLog.WriteLine(string.Format("Mesh #{0:000}", i));
+				DebugLog.WriteLine(mesh.ToString());
+				i++;
+			}
 
 			DebugLog.End();
 		}
